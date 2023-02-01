77adacd606ed8bc9f06018e359a2c819fd2bb818
peter.norman1 <peternormandev@gmail.com>
2023-01-31 23:36:16 -0500

    first commit

---
+++ wp-content/themes/sampsonsound/functions.php
@@ -0,0 +1,19 @@
+
+<?php
+
+
+function sampson_theme_enqueue_styles() {
+    //parent styles
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    // gulp compiled main.css
+    $modified = date('YmdHi', filemtime(get_stylesheet_directory() . '/dist/css/main.css'));
+    wp_enqueue_style('main', get_stylesheet_directory_uri() . '/dist/css/main.css', array('parent-style'), $modified);
+}
+add_action( 'wp_enqueue_scripts', 'sampson_theme_enqueue_styles' );
+
+function sampson_theme_enqueue_scripts() {
+    // gulp compiled main.js
+    wp_enqueue_script('main-js', get_stylesheet_directory_uri() . '/dist/js/main.js', false, '', true);
+}
+add_action( 'wp_enqueue_scripts', 'sampson_theme_enqueue_scripts' );
+?>
---
+++ wp-content/themes/sampsonsound/page-front.php
@@ -0,0 +1,14 @@
+<?php
+	/**
+	 * Template Name: Front Page
+	 *
+	 * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+	 *
+	 */
+	
+	get_header();
+?>	
+<?php get_template_part( 'template-parts/header' ); ?>
+<?php get_template_part( 'template-parts/footer' ); ?>
+<?php
+get_footer();
